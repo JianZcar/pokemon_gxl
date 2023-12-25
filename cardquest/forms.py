fcaffc2208cecdecf14a6f0d5c74262b0412c30e
JianZcar <jianstbn@gmail.com>
2023-12-25 21:44:24 +0800

    added calendar for birthdate

--- cardquest/forms.py
+++ cardquest/forms.py
@@ -4,6 +4,16 @@ from .models import Trainer, Collection, PokemonCard
 
 
 class TrainerForm(forms.ModelForm):
+    birthdate = forms.DateField(
+        widget=forms.DateInput(
+            format='%Y-%m-%d',
+            attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }
+        )
+    )
+
     class Meta:
         model = Trainer
         fields = '__all__'
